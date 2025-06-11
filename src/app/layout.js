c08825003db01c30f3814d219f94fc1fadb79310
Purushottam912000 <66175735+Purushottam912000@users.noreply.github.com>
2025-06-12 00:20:14 +0530

    Added more related files for auth and header css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
 import "./globals.css";
+import Header from "@/components/Header";
+import { getAuthenticatedAppForUser } from "@/lib/firebase/serverApp"; 
 
 export const metadata = {
   title: "Indian Burger | Vadapav",
   description: "Indian burger means vadapav.",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const { currentUser } = await getAuthenticatedAppForUser();
   return (
     <html lang="en">
       <body>
+        <Header initialUser={currentUser?.toJSON()} />
         {children}
       </body>
     </html>

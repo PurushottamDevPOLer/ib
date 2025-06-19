4e6626ec6e0f9a21ccc972038a003b22879a46e8
Purushottam912000 <66175735+Purushottam912000@users.noreply.github.com>
2025-06-19 13:39:40 +0530

    changes in cart file

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -3,6 +3,11 @@
 import BuyProduct from '@/components/razorpay/BuyProduct';
 import { useCartStore } from '@/store/cartStore';
 
+import React, { Suspense } from "react";
+import Buy from "./Buy";
+// import { useRouter } from 'next/navigation';
+import Loading from "@/app/loading";
+
 export default function CartPage() {
   const cart = useCartStore((state) => state.cart);
   const updateQuantity = useCartStore((state) => state.updateQuantity);
@@ -75,7 +80,12 @@ export default function CartPage() {
               >
                 Place Order
               </button>
-              <BuyProduct />
+
+            </div>
+            <div>
+              <Suspense fallback={<Loading />}>
+                <Buy makePayment={makePayment} />
+              </Suspense>
             </div>
           </>
         )}
